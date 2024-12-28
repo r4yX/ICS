5d165ea17c037d5d1e16ee4d52bb0258af35a6c0
syltr1x <syltr1x@proton.me>
2024-12-28 14:02:14 +0000

    fix: database functions import & commands rename

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,26 +1,27 @@
-use database;
+mod database;
+use database::*;
 
 #[tauri::command]
-fn insert_budget(customer: &str, vehicle: &str, concept: &str, kilometrage: f32, total: f32) {
-   database::insert_budget(customer, vehicle, concept, kilometrage, total).unwrap();
+fn create_budget(customer: &str, vehicle: &str, concept: &str, kilometrage: f32, total: f32) {
+   insert_budget(customer, vehicle, concept, kilometrage, total).unwrap();
 }
 #[tauri::command]
 fn create_customer(name: &str, phone: &str, cuil: &str, dni: &str, tipo: &str, vehicles: Vec<&str>) {
     let _ = database::insert_customer(name, phone, cuil, dni, tipo);
     for vehicle in vehicles.iter() {
-        database::update_vehicle(vehicle, name);
+        update_vehicles(vehicle, name);
     }
 }
 #[tauri::command]
-fn insert_item(id: &str, name: &str, price: &str, tipo: &str, manufacturer: &str, supplier: &str, model: &str, stock: &str) {
-    database::insert_item(id, name, price, tipo, manufacturer, supplier, model, stock)
+fn create_item(id: &str, name: &str, price: f32, tipo: &str, manufacturer: &str, supplier: &str, model: &str, stock: u16) {
+    insert_item(id, name, price, tipo, manufacturer, supplier, model, stock)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![insert_budget, create_customer, insert_item])
+        .invoke_handler(tauri::generate_handler![create_budget, create_customer, create_item])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
