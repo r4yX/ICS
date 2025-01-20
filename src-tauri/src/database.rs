771b7623eda1a1334e1fa813157b129dee2d72fd
syltr1x <syltr1x@proton.me>
2025-01-20 08:34:19 -0300

    del: db unused function

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -391,18 +391,3 @@ pub fn insert_history(id: &str, order: Order, date: &str) -> Result<String, Stri
         Err(e) => Err(format!("Err ({}) writing history", e)),
     }
 }
-
-// Delete Functions
-pub fn delete_budget(id: &str) -> Result<String, String> {
-    let conn = match Connection::open("/home/syltr1x/work_dir/Punto_Diesel/src/debug.db") {
-        Ok(conn) => conn,
-        Err(_) => return Err("Failed to open database connection".to_string()),
-    };
-    match conn.execute(
-        "DELETE FROM budgets WHERE id=?1",
-        params![id],
-    ) {
-        Ok(_) => Ok(format!("budget {} removed successfully", id)),
-        Err(_) => Err(format!("Error deleting budget {}", id)),
-    }
-}
